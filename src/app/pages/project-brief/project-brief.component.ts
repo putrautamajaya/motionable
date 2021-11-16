76326066653c84a43c60cf7785ccad6a679790c1
putrautamajaya <putra.utamajaya@ematicsolutions.com>
2021-11-16 14:08:35 +0700

    project brief step 1 view

--- src/app/pages/project-brief/project-brief.component.ts
+++ src/app/pages/project-brief/project-brief.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectBriefComponent implements OnInit {
 
-  constructor() { }
+  step: number
+
+  progressBar: {active: number[], inactive: number[]}
+
+  constructor() {
+    this.step = 1
+    this.setProgressBar()
+  }
 
   ngOnInit(): void {
+  }
+
+  setProgressBar() {
+    this.progressBar = {
+      active: [],
+      inactive: []
+    }
+
+    for (let i=0; i<this.step; i++) {
+      this.progressBar.active.push(i)
+    }
+
+    for (let i=this.step; i<5; i++) {
+      this.progressBar.inactive.push(i)
+    }
   }
 
 }
